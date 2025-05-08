1386aedb904f923f08d8bda893663d009a10c5bc
lunaxq <166347982+lunaxq@users.noreply.github.com>
2025-05-08 10:29:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 技术栈
+React + TypeScript + Vite
+
+## css 方案
+css modules
+
+# 运行项目
+## 安装依赖
+```js
+pnpm install
+```
+
+## 启动项目
+```js
+pnpm dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import classes from './App.module.css'
+
+function App() {
+  return <div className={classes.container}>动态表单</div>
+}
+
+export default App
